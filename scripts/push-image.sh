17ad73f42ab6fb1a932c2b03a96edbdeaffa0196
Faisal <mohdfaisalmujahidfaisal@gmail.com>
2024-09-11 13:33:27 +1000

    Set executable permissions

--- scripts/push-image.sh
+++ scripts/push-image.sh
@@ -1,23 +1,20 @@
-#
-# Publishes a Docker image.
-#
-# Environment variables:
-#
-#   CONTAINER_REGISTRY - The hostname of your container registry.
-#   REGISTRY_UN - User name for your container registry.
-#   REGISTRY_PW - Password for your container registry.
-#   VERSION - The version number to tag the images with.
-#
-# Usage:
-#
-#       ./scripts/push-image.sh
-#
-
 set -u # or set -o nounset
+set -x # Enable debugging
+
+# Check that variables are set
 : "$CONTAINER_REGISTRY"
 : "$VERSION"
 : "$REGISTRY_UN"
 : "$REGISTRY_PW"
 
-echo $REGISTRY_PW | docker login $CONTAINER_REGISTRY --username $REGISTRY_UN --password-stdin
-docker push $CONTAINER_REGISTRY/video-streaming:$VERSION
+# Debug output to ensure variables are correctly passed
+echo "CONTAINER_REGISTRY: $CONTAINER_REGISTRY"
+echo "VERSION: $VERSION"
+echo "REGISTRY_UN: $REGISTRY_UN"
+# Do not echo password for security reasons
+
+# Docker login
+echo $REGISTRY_PW | docker login $CONTAINER_REGISTRY --username $REGISTRY_UN --password-stdin || exit 1
+
+# Push the Docker image
+docker push $CONTAINER_REGISTRY/video-streaming:$VERSION || exit 1
